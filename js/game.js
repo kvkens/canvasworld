746491549bfdd0ad9bcbee325e4264cd25a37b3b
JayChou <kvkens@qq.com>
2016-03-30 17:28:53 +0800

    update

--- js/game.js
+++ js/game.js
@@ -77,35 +77,86 @@ var Game = new function() {
 	}
 	//星星
 var Starfield = function(speed, opacity, numStars, clear) {
-	var stars = document.createElement("canvas");
-	stars.width = Game.width;
-	stars.height = Game.height;
-	var starCtx = stars.getContext("2d");
-	var offset = 0;
-	if (clear) {
-		starCtx.fillStyle = "#000";
-		starCtx.fillRect(0, 0, stars.width, stars.height);
+		var stars = document.createElement("canvas");
+		stars.width = Game.width;
+		stars.height = Game.height;
+		var starCtx = stars.getContext("2d");
+		var offset = 0;
+		if (clear) {
+			starCtx.fillStyle = "#000";
+			starCtx.fillRect(0, 0, stars.width, stars.height);
+		}
+		starCtx.fillStyle = "#FFF";
+		starCtx.globalAlpha = opacity;
+		for (var i = 0; i < numStars; i++) {
+			starCtx.fillRect(Math.floor(Math.random() * stars.width), Math.floor(Math.random() * stars.height), 2, 2);
+		}
+		this.draw = function(ctx) {
+			var intOffset = Math.floor(offset);
+			var remaining = stars.height - intOffset;
+
+			if (intOffset > 0) {
+				ctx.drawImage(stars, 0, remaining, stars.width, intOffset, 0, 0, stars.width, intOffset);
+				//console.log("ctx.drawImage(stars, 0, %s, %s, %s, 0, 0, %s, %s);",remaining,stars.width,intOffset,stars.width,intOffset);			
+			}
+			if (remaining > 0) {
+				ctx.drawImage(stars, 0, 0, stars.width, remaining, 0, intOffset, stars.width, remaining);
+				//console.log("ctx.drawImage(stars, 0, 0, %s, %s, 0, %s, %s, %s)",stars.width,remaining,intOffset,stars.width,remaining);
+			}
+		}
+		this.step = function(dt) {
+			offset += dt * speed;
+			offset = offset % stars.height;
+		}
 	}
-	starCtx.fillStyle = "#FFF";
-	starCtx.globalAlpha = opacity;
-	for (var i = 0; i < numStars; i++) {
-		starCtx.fillRect(Math.floor(Math.random() * stars.width), Math.floor(Math.random() * stars.height), 2, 2);
+	//画文字
+var TitleScreen = function TitleScreen(title, subtitle, callback) {
+	this.step = function(dt) {
+		if (Game.keys["fire"] && callback) {
+			callback();
+		}
 	}
 	this.draw = function(ctx) {
-		var intOffset = Math.floor(offset);
-		var remaining = stars.height - intOffset;
-		if (intOffset > 0) {
-			ctx.drawImage(stars, 0, remaining, stars.width, intOffset, 0, 0, stars.width, intOffset);
-		}
-		if (remaining > 0) {
-			ctx.drawImage(stars, 0, 0, stars.width, remaining, 0, intOffset, stars.width, remaining);
-		}
-	}
-	this.step = function(dt) {
-		offset += dt * speed;
-		offset = offset % stars.height;
+		ctx.fillStyle = "#FFFFFF";
+		ctx.textAlign = "center";
+		ctx.font = "bold 40px bangers";
+		ctx.fillText(title, Game.width / 2, Game.height / 2);
+		ctx.font = "bold 20px bangers";
+		ctx.fillText(subtitle, Game.width / 2, Game.height / 2 + 40);
 	}
 }
+
+//飞船
+var PlayerShip = function() {
+	this.w = SpriteSheet.map["ship"].w;
+	this.h = SpriteSheet.map["ship"].h;
+	this.x = Game.width / 2 - this.w / 2;
+	this.y = Game.height - 10 - this.h;
+	this.vx = 0;
+	this.step = function(dt) {
+		this.maxVel = 200;
+		this.step = function(dt) {
+			if (Game.keys["left"]) {
+				this.vx = -this.maxVel;
+			} else if (Game.keys["right"]) {
+				this.vx = this.maxVel;
+			}
+
+			this.x += this.vx * dt;
+			console.log(this.x);
+			if (this.x < 0) {
+				this.x = 0;
+			} else if (this.x > Game.width - this.w) {
+				this.x = Game.width - this.w;
+			}
+		}
+	}
+	this.draw = function(ctx) {
+		SpriteSheet.draw(ctx, "ship", this.x, this.y, 0);
+	}
+
+}
+
 var sprites = {
 	ship: {
 		sx: 1,
@@ -120,7 +171,10 @@ var startGame = function() {
 	Game.setBoard(0, new Starfield(20, 0.4, 100, true));
 	Game.setBoard(1, new Starfield(50, 0.6, 100));
 	Game.setBoard(2, new Starfield(100, 1.0, 50));
-
+	Game.setBoard(3, new TitleScreen("星球大战", "一个相当刺激的游戏", playGame));
+}
+var playGame = function() {
+	Game.setBoard(3, new PlayerShip());
 }
 window.addEventListener("load", function() {
 	Game.initialize("game", sprites, startGame);
